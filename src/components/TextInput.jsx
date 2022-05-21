c395eaeeeec558ed3fe6a6f2dd179d8ba8be835e
pratik-k <prakar07@protonmail.com>
2022-05-21 22:51:47 +0530

    make textInput component

---
+++ src/components/TextInput.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class TextInput extends Component {
+  render() {
+    let styleClass=""
+    switch (this.props.st) {
+        case "preview":
+            styleClass="input-preview"
+            break;
+        case "edit":
+            styleClass="input-edit"
+            break;
+        default:
+            break;
+    }
+    return (
+      <input type="text" className={`${styleClass} ${this.props.className}`} placeholder={this.props.placeholder} />
+    )
+  }
+}
