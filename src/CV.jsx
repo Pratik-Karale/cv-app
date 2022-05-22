cf350823eacf68a23a49667be0fad8f2f384e301
pratik-k <prakar07@protonmail.com>
2022-05-22 09:43:32 +0530

    import info section and give pre/edit status to all inputs

--- src/CV.jsx
+++ src/CV.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Header from "./components/Header"
 import Description from './components/Description'
-
+import InfoSection from './components/InfoSection'
 export default class CV extends Component {
     constructor(props){
       super(props)
@@ -14,7 +14,8 @@ export default class CV extends Component {
       <div className="CV">
           <div>
               <Header st={this.state.mode}/>
-              <Description className="" placeholder="okokokookokooko"/>
+              <Description st={this.state.mode} className="" placeholder="okokokookokooko"/>
+              <InfoSection st={this.state.mode}/>
           </div>
       </div>
     )
