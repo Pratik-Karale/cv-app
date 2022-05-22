f2d4d4580d7562f91ad87487d3589fad03d5672e
pratik-k <prakar07@protonmail.com>
2022-05-22 13:43:11 +0530

    add InfoSection component

---
+++ src/components/InfoSection.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import TextInput from './TextInput'
+export default class InfoSection extends Component {
+    constructor(props){
+        super(props)
+        this.state={bulletPoints:["edrfwerwer"]}
+        this.addBulletPoint=this.addBulletPoint.bind(this)
+        this.editBulletPoint=this.editBulletPoint.bind(this)
+    }
+    addBulletPoint(){
+        this.setState({bulletPoints:[...this.state.bulletPoints,""]})
+    }
+    editBulletPoint(e){
+        console.log(this)
+        const bulletPointIndex=e.target.key
+        this.state.bulletPoints[bulletPointIndex]=e.target.value
+        this.setState({bulletPoints:[...this.state.bulletPoints,"ok"]})
+    }
+  render() {
+    //   console.log(this.editBulletPoint)
+    return (
+      <div className="Info-section">
+          <TextInput value="wewwew" st="preview"/>
+          <ul>
+            {this.state.bulletPoints.map((bulletPoint,i)=>{
+                return <li><TextInput st={this.props.st} editInput={this.editBulletPoint} value={bulletPoint}/></li>
+            })}
+          </ul>
+          <button onClick={this.addBulletPoint}>Add</button>
+      </div>
+    )
+  }
+}
